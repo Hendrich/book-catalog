80b21fab4a62c2a7c775ab5505734a51d6044713
hendrich2404 <hendri.christianto@finansia.com>
2025-05-15 10:44:57 +0700

    menambahkan .env di gitignore

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,20 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
+/// ****** koneksi local *******///
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
 
+/// ****** koneksi Railway *******///
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 module.exports = pool;
